a622b1eb5445c89e95a35bf7db91e62be5c024ce
DJA <DJA@DJA-PC>
2017-11-21 18:31:44 +0900

    project initiation

---
+++ exam/src/p01/Exam01.java
@@ -0,0 +1,17 @@
+package p01;
+
+public class Exam01 {
+	
+	public static void main(String[] args) {
+		
+		float a =3;
+		float b =4;
+		System.out.println(a*b);
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a/b);
+	}
+	
+}
+
+
