265e56b59bd31b70c25f39307fdcc792d1ebd19b
YUZINISM <51945390+YUZINISM@users.noreply.github.com>
2019-07-15 17:36:06 +0900

    Update FunctionTest.java

--- exam/src/p02/FunctionTest.java
+++ exam/src/p02/FunctionTest.java
@@ -1,19 +1,17 @@
-package p02;
+package test;
 
 public class FunctionTest {
+	static void add(int num1, int num2) {
+		System.out.println(num1+num2);
+	}
+	static int add2(int num1,int num2) {
+		return num1+num2;
+	}
 	
-
-	
-		static void add(int num1, int num2) {
-			System.out.println(num1+num2);
-		}
-	
-		static int add2(int num1,int num2) {
-			return num1+num2;
-		}
-	
-		System.out.println(add2(1,2))
-	
-	
+	public static void main(String[] args) {
+		
+		FunctionTest f = new FunctionTest();
+		
+		System.out.println(f.add2(1,2));
+	}
 }
-
